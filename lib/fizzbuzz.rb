cf86afae47aff1b3157e2c92315b828e15ad9468
marcoaam <marco.army@gmail.com>
2014-06-28 00:12:33 +0100

    Last version of fizzbuzz until june 27

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,15 @@
 def is_divisible_by_three?(number)
 	number % 3 == 0
 end
+
+def is_divisible_by_five?(number)
+	number % 5 == 0
+end
+
+def is_divisible_by_fifteen?(number)
+	number % 15 == 0
+end
+
+def is_divisible_by?(number, divisor)
+	number % divisor == 0
+end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -7,4 +7,16 @@ describe 'FizzBuzz'  do
 	it 'knows that 3 is not divisible by 1' do
 		expect(is_divisible_by_three?(1)).to be false
 	end
+	it 'Knows that 5 is divisible by five' do
+		expect(is_divisible_by_five?(5)).to be true
+	end
+	it 'Knows that 1 is not divisible by five' do
+		expect(is_divisible_by_five?(1)).to be false
+	end
+	it 'knows that 15 is divisible by fifteen' do
+		expect(is_divisible_by_fifteen?(15)).to be true
+	end
+	it 'Knows that 1 is not divisible by fifteen' do
+		expect(is_divisible_by_fifteen?(1)).to be false
+	end
 end
